04969c53e4ed4c43fdb35e94ec14dd491d45ec9e
Nda-cunh <hydraldev@gmail.com>
2024-10-12 10:08:04 +0200

    trim useless space

--- install.sh
+++ install.sh
@@ -12,14 +12,14 @@ echo ''
 
 # install meson or ninja if not exist
 
-if ! [ -x "$(command -v meson)" ] || ! [ -x "$(command -v ninja)" ]; then 
+if ! [ -x "$(command -v meson)" ] || ! [ -x "$(command -v ninja)" ]; then
 	echo "meson not found, installing..."
 	pip install meson ninja --user
 fi
 
 export PATH=$PATH:$HOME/.local/bin
 
-if ! [ -x "$(command -v meson)" ] || ! [ -x "$(command -v ninja)" ]; then 
+if ! [ -x "$(command -v meson)" ] || ! [ -x "$(command -v ninja)" ]; then
 	echo "meson install failed, please install manually"
 	echo ""
 	echo "	Ubuntu    (apt install meson ninja-build)"
@@ -27,7 +27,7 @@ if ! [ -x "$(command -v meson)" ] || ! [ -x "$(command -v ninja)" ]; then
 	echo "	ArchLinux (pacman -S meson ninja)"
 	exit 1
 fi
-	
+
 # build if build/build.ninja doesnt exist
 
 if [ ! -f build/build.ninja ]; then
